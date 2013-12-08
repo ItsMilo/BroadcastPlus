5a7efa37b698d854141a5208b7b9f397ea320d49
AlexKvazos <alexkvazos@hotmail.com>
2013-12-08 14:51:33 -0800

    Plugin release

---
+++ src/com/alexkvazos/broadcastplus/BroadcastPlus.java
@@ -0,0 +1,38 @@
+package com.alexkvazos.broadcastplus;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BroadcastPlus extends JavaPlugin{
+
+	//Actions to perform when enabled/disabled
+	public void onEnable(){}
+	public void onDisable(){}
+	
+	
+	
+	//Command Handler
+		public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+			if (cmd.getName().equalsIgnoreCase("bcplus") && sender.hasPermission("broadcastplus.use") && args.length >=2) {
+					ArrayList<String> arguments = new ArrayList<String>(Arrays.asList(args));
+					
+					//Array Builder
+					String text = "";
+					for(int x = 1; x < arguments.size(); x++){
+					text += " " + arguments.get(x);
+					}
+					text=text.replaceAll("&([a-z0-9])","§$1");
+					Bukkit.broadcastMessage(ChatColor.GOLD+"["+ChatColor.GREEN+args[0]+ChatColor.GOLD+"] "+text);
+				return true;
+			}
+		return false;
+		}
+		//bcplus prefix rest of the message this is the broadcast
+}
+
